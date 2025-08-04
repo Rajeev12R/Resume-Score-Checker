9e4621b111ef64127c4e32e6504e1007c409eed5
Sai Prashanth <saiprashanth751@gmail.com>
2025-08-04 12:30:04 +0530

    Update main.py
    changed-backend-url

--- server/main.py
+++ server/main.py
@@ -155,7 +155,7 @@ app = FastAPI()
 # Allow CORS from your frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://resume-ai-score-checker.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
